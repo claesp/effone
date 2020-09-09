7c9cffcd6d69838d9ff9ce5d52388cce974307d0
Claes Persson <c@cp.se>
2020-09-09 23:11:02 +0200

    Lobby Info specification.

--- libeffone/libeffone.go
+++ libeffone/libeffone.go
@@ -300,3 +300,17 @@ type PacketFinalClassificationData struct {
 	NumCars            uint8        // Number of cars in the final classification
 	ClassificationData [22]FinalClassificationData
 }
+
+type LobbyInfoData struct {
+	AIControlled uint8     // Whether the vehicle is AI (1) or Human (0) controlled
+	TeamID       uint8     // Team id - see appendix (255 if no team currently selected)
+	Nationality  uint8     // Nationality of the driver
+	Name         [48]uint8 // Name of participant in UTF-8 format – null terminated // Will be truncated with ... (U+2026) if too long
+	ReadyStatus  uint8     // 0 = not ready, 1 = ready, 2 = spectating
+}
+
+type PacketLobbyInfoData struct {
+	Header       PacketHeader // Header
+	NumPlayers   uint8        // Number of players in the lobby data
+	LobbyPlayers [22]LobbyInfoData
+}
