101c021af126e115e8036817e92ef8458494a0aa
Claes Persson <c@cp.se>
2020-09-15 00:42:44 +0200

    Adding web sockets support, based on Chat example. Not currently wired to the rest.

--- effone.go
+++ effone.go
@@ -4,54 +4,224 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
+	"net/http"
+	"time"
 
 	f1 "github.com/claesp/effone/libeffone"
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	addr, err := net.ResolveUDPAddr("udp", ":20777")
-	if err != nil {
-		fmt.Println("Wrong Address")
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+// Client is a middleman between the websocket connection and the hub.
+type Client struct {
+	hub *Hub
+
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
 		return
 	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
 
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	// Allow collection of memory referenced by the caller by doing all work in
+	// new goroutines.
+	go client.writePump()
+	go client.readPump()
+}
+
+func main() {
 	go func() {
+		addr, err := net.ResolveUDPAddr("udp", ":20777")
+		if err != nil {
+			fmt.Println("Wrong Address")
+			return
+		}
+
 		conn, err := net.ListenUDP("udp", addr)
 		if err != nil {
 			fmt.Println(err)
 		}
 		defer conn.Close()
 
-		fmt.Println("listening on", addr)
+		fmt.Println("listening on", addr, "for F1 2020 telemetry")
 		for {
-			handle(conn)
+			handleTelemetry(conn)
 		}
 	}()
 
-	addr2, err := net.ResolveUDPAddr("udp", ":20778")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	hub := newHub()
+	go hub.run()
 
-	conn2, err := net.ListenUDP("udp", addr2)
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	fmt.Println("listening on :8080 for web requests")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println(err)
-	}
-	defer conn2.Close()
-
-	fmt.Println("listening on", addr2)
-	for {
-		handle(conn2)
+		log.Fatalln(err)
 	}
 }
 
-func handle(conn *net.UDPConn) {
-	read(conn)
+func handleTelemetry(conn *net.UDPConn) {
+	readTelemetry(conn)
 }
 
-func read(conn *net.UDPConn) {
+func readTelemetry(conn *net.UDPConn) {
 	tmp := make([]byte, 4096)
 	conn.Read(tmp)
 	buf := bytes.NewBuffer(tmp)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/claesp/effone
 
 go 1.15
+
+require github.com/gorilla/websocket v1.4.2 // indirect
